dc08d5d99c5a5d78d35b094e16d23cb5bdf6835a
Lucky Icban <lddicban@gmail.com>
2023-07-31 23:50:19 +0800

    registering users

--- users/forms.py
+++ users/forms.py
@@ -11,7 +11,7 @@ class UserRegistrationForm(forms.ModelForm):
     password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput)
     class Meta:
         model = User
-        fields={'username', 'email', 'first_name'}
+        fields={ 'first_name', 'username', 'email' }
 
     def check_password(self):
         if self.cleaned_data['password'] != self.cleaned_data['password2']:
