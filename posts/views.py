c1ff4d4915b343056aff1b93b165f26eb088db85
Lucky Icban <lddicban@gmail.com>
2023-08-03 23:36:00 +0800

    created post

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .forms import PostCreateForm
 
 # Create your views here.
+
+def post_create(request):
+    if request.method == 'POST':
+        form = PostCreateForm(date=request.POST)
+        if form.is_valid():
+            new_item = form.save(commit=False)
+            new_item.user = request.user
+            new_item.save()
+    else:
+        form = PostCreateForm(date=request.GET)
+    return render(request, 'post/create.html', {'form': form})
+        
